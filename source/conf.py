00921d460910eb44c1b45787a06f500a91890d34
Cwohaocai <1450789009@qq.com>
2024-07-27 10:40:45 +0800

    first commit

--- source/conf.py
+++ source/conf.py
@@ -6,7 +6,7 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'MaterPi-4B'
+project = 'TurboPi-4B'
 copyright = '2024, Hiwonder'
 author = 'Hiwonder'
 release = 'v1.0'
